f583f5c05017db117f4855d67330cd5a209294d2
Meshach Odejide <meshacholuwadamilare@gmail.com>
2023-01-30 12:48:39 +0100

    set up for database

---
+++ setup/setup.go
@@ -0,0 +1,20 @@
+package setup
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Connection() {
+	dsn := "root:secret@tcp(mysql:3306)/go-docker?charset=utf8&parseTime=True&loc=Local"
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+func GetDB() *gorm.DB {
+	return db
+}
