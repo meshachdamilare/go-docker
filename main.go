19425faf34afbd121a0179f400e585d3b5e89086
Meshach Odejide <meshacholuwadamilare@gmail.com>
2023-01-30 12:53:10 +0100

    main.go updated

--- main.go
+++ main.go
@@ -1,62 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"html/template"
-	"io"
+	"github.com/gorilla/mux"
+	"github.com/meshachdamilare/go-docker/router"
+	"github.com/meshachdamilare/go-docker/setup"
 	"log"
 	"net/http"
 )
 
-type post struct {
-	UserID int    `json:"userID"`
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Body   string `json:"body"`
-}
-
-var form = `
-<h1>Post #{{.ID}}</h1>
-<div>{{printf "The User %d" .UserID}}</div>
-<div>{{printf "Title is %s" .Title}}</div>
-<div>{{printf "Body is  %s" .Body}}</div>`
-
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	const baseUrl = "https://jsonplaceholder.typicode.com/"
-	resp, err := http.Get(baseUrl + r.URL.Path[1:])
-
-	if err != nil {
-		//signal an error with http.Error
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
-	}
-
-	//ensure to close the body that was open else server runs out of sockets
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		//signal an error with http.Error
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
-	}
-
-	var item post
-
-	err = json.Unmarshal(body, &item)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
-	}
-
-	_template := template.New("sample")
-	_template.Parse(form)
-	_template.Execute(w, item)
+	setup.Connection()
+	//db := setup.GetDB()
+	r := mux.NewRouter()
+	router.Register(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
